53f18fb5cd46799e239fb0ca669c4246c431d346
James Brooks <brooksjamesk01@gmail.com>
2024-05-29 20:01:08 -0700

    added database connection to app.py

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from models import db, User
+import os
 
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+migrate = Migrate(app,db)
+
 @app.route("/")
 def index():
     return render_template("index.html")
+ 
