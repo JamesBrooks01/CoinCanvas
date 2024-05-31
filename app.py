fe2a9eda608fa774088ae0889e99ac2059b88af0
James Brooks <brooksjamesk01@gmail.com>
2024-05-30 23:14:45 -0700

    added Auth0 capability for Google Login

--- app.py
+++ app.py
@@ -1,46 +1,50 @@
-from flask import Flask, render_template, request, jsonify, make_response
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request, jsonify, make_response, redirect, session, url_for
 from flask_migrate import Migrate
 from models import db, User
 import os
+from urllib.parse import quote_plus, urlencode
+from authlib.integrations.flask_client import OAuth
 
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = os.environ.get("APP_SECRET_KEY")
+
+oauth = OAuth(app)
+
+oauth.register(
+    "auth0",
+    client_id=os.environ.get("AUTH0_CLIENT_ID"),
+    client_secret=os.environ.get("AUTH0_CLIENT_SECRET"),
+    client_kwargs={
+        "scope": "openid profile email",
+    },
+    server_metadata_url=f'https://{os.environ.get("AUTH0_DOMAIN")}/.well-known/openid-configuration'
+)
 
 db.init_app(app)
 migrate = Migrate(app,db)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
- 
+    data = session.get('user')
+    return render_template("index.html", session=data)
 
-@app.route('/login', methods = ['POST', 'GET'])
+
+
+@app.route("/login")
 def login():
-    if request.method == 'GET':
-        return "Login via the login Form"
-     
-    if request.method == 'POST':
-        email = request.form['email']
-        query = request.form['query']
-        formatted_query = [query]
-        new_user = User(user_email=email, user_queries=formatted_query)
-        db.session.add(new_user)
-        db.session.commit()
-        return f"Done!!"
-    
-@app.route('/create')
-def create():
-    email = request.form['email']
-    query = request.form['query']
-    formatted_query = [query]
-    new_entry = User(user_email=email, user_queries=formatted_query)
-    db.session.add(new_entry)
-    db.session.commit()
-    return make_response('', 201)
+    return oauth.auth0.authorize_redirect(
+        redirect_uri=url_for("callback", _external=True)
+    )
+
+@app.route("/callback")
+def callback():
+    token = oauth.auth0.authorize_access_token()
+    session['user'] = token
+    return redirect('/')
 
 @app.route('/delete')
 def delete():
@@ -60,3 +64,21 @@ def update():
     result.user_queries = new_array
     db.session.commit()
     return make_response('', 201)
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(
+        "https://" + os.environ.get("AUTH0_DOMAIN")
+        + "/v2/logout?"
+        + urlencode(
+            {
+                "returnTo": url_for("index", _external=True),
+                "client_id": os.environ.get("AUTH0_CLIENT_ID"),
+            },
+            quote_via=quote_plus,
+        )        
+    )
+
+
+
