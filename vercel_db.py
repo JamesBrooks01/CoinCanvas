17ef0533b5056c206430191463d58b1f5e9edc5e
James Brooks <brooksjamesk01@gmail.com>
2024-06-07 13:11:58 -0700

    removed delete user function and admin helper function from vercel_db.py

--- vercel_db.py
+++ vercel_db.py
@@ -5,20 +5,11 @@ def get_data(user):
     data = db.session.execute(db.select(User).filter_by(user_email=user)).scalar_one_or_none()
     return data
 
-def get_all_users():
-    data = db.session.execute(db.select(User)).all()
-    return data
-
 def create_user(user):
     formatted_query = []
     new_user = User(user_email=user, user_queries=formatted_query)
     db.session.add(new_user)
     db.session.commit() 
-
-def delete_user(user):
-    result = User.query.filter_by(user_email=user).first()
-    db.session.delete(result)
-    db.session.commit()
 
 def update_user(user,query):
     db.session.execute(db.update(User).where(User.user_email==user).values(user_queries=query))
