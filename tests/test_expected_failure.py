2546aca8d0ca644c26ccaeaca65c0a619b3c4e18
James Brooks <brooksjamesk01@gmail.com>
2024-06-11 16:24:39 -0700

    altered inital tests and added tests for expected failure states for the routes that could be pytested.

--- tests/test_expected_failure.py
+++ tests/test_expected_failure.py
@@ -1,10 +1,44 @@
 import pytest
 import requests
+import os
+from dotenv import load_dotenv
 
-def test_update():
-  base_url = 'http://127.0.0.1:5000'
+load_dotenv()
+
+production_user= os.environ.get('TEST_USER')
+app_url = os.environ.get('APP_URL')
+
+# @pytest.mark.skip("TODO")
+def test_update_invalid_auth():
   auth = 2
   headers = {"Authorization": f"Bearer {auth}"}
-  data = {'query': "BTC", 'user': 'brooksjamesk01@gmail.com', 'type': 'Crypto'}
-  actual = requests.post(f'{base_url}/update',data=data,headers=headers)
+  data = {'query': "BTC", 'user': production_user, 'type': 'Crypto'}
+  actual = requests.post(f'{app_url}/update',data=data,headers=headers)
   assert actual.status_code == 401 
+
+# @pytest.mark.skip("TODO")
+def test_delete():
+  data = {'delete': "BTC",}
+  actual = requests.post(f'{app_url}/delete',data=data)
+  assert actual.status_code == 400
+
+# @pytest.mark.skip("TODO")
+def test_new_query():
+  data = {'query': "BTC", 'type': "Crypto"}
+  actual = requests.post(f'{app_url}/api',data=data)
+  assert actual.status_code == 400
+
+def test_update_invalid_user():
+  auth = os.environ.get('APP_SECRET_KEY')
+  headers = {"Authorization": f"Bearer {auth}"}
+  data = {'query': "BTC", 'user': 'invalid@email.com', 'type': 'Crypto'}
+  actual = requests.post(f'{app_url}/update',data=data,headers=headers)
+  assert actual.status_code == 400
+
+# @pytest.mark.skip("TODO")
+def test_update_invalid_type():
+  auth = os.environ.get('APP_SECRET_KEY')
+  headers = {"Authorization": f"Bearer {auth}"}
+  data = {'query': "BTC", 'user': production_user, 'type': 'Invalid'}
+  actual = requests.post(f'{app_url}/update',data=data,headers=headers)
+  assert actual.status_code == 400 
